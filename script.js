f1b14d7bfadcb5647570640ba108f5e776a058e4
Juan Portem <juanluis.portem@gmail.com>
2022-04-28 12:05:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var clip = document.getElementById("mainvideo");
+
+function playVideo() {
+      clip.play();
+}
+
+function pauseVideo() {
+      clip.pause();
+}
